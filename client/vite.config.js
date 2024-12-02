10e3c8c0b31ca61d2503fd2dd9eceae43d1da44a
cryptoleonardo1 <cryptoleonardo1@gmail.com>
2024-12-02 12:52:07 +0200

    test

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',  // This tells Vite to output to a 'build' directory
+    sourcemap: true
+  },
   server: {
     port: 3000,
     proxy: {
